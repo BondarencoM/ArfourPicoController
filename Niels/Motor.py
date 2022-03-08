58f78ee144365f64d2e7e62c1888af97fadd8208
Nielsvhb99 <nielsvanhinsberg@gmail.com>
2022-03-08 14:02:24 +0100

    added classes

---
+++ Niels/Motor.py
@@ -0,0 +1,11 @@
+from digitalio import DigitalInOut
+from gpiozero import PWMOutputDevice, Motor
+import pwmio
+import board
+import rotaryio
+import digitalio
+
+class Motor():
+
+    def init(self):
+
