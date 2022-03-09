82d7796defb368b5c01312316246d9ba3e4dc851
Quinvey Etienne <quinvey@hotmail.com>
2022-03-09 16:47:41 +0100

    Save first comms
    Save of the first working comms?

--- src/boot.py
+++ src/boot.py
@@ -1,5 +1,3 @@
-# The imports of all used libraries
 import usb_cdc
 
-## Setup
-serial = usb_cdc.enable(console = False, data = True)
+usb_cdc.enable(True, True)
--- src/code.py
+++ src/code.py
@@ -1,15 +1,17 @@
 # The imports of all used libraries
 import IODefinitions
+import time
 import usb_cdc
 
 ## Setup
-serial = usb_cdc.Serial
-
-
-## Single run
-print("test: " )
-print(not not serial.connected)
+serial = usb_cdc.console
 
 ## Loop
 while True:
-    pass  # does nothing
+    if serial.connected:
+        if serial.in_waiting:
+            print(serial.connected)
+            print(serial.in_waiting)
+            serial.read(serial.in_waiting)
+
+    time.sleep(1)
