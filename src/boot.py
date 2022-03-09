a7c61c11470c62ca375c81b43eb66baac8c43613
Quinvey Etienne <quinvey@hotmail.com>
2022-03-09 18:03:30 +0100

    mockup
    extra comments and a mockup of next steps

--- src/boot.py
+++ src/boot.py
@@ -1,3 +1,5 @@
+# The imports of all used libraries
 import usb_cdc
 
+## Setup
 usb_cdc.enable(True, True)
--- src/code.py
+++ src/code.py
@@ -8,10 +8,20 @@ serial = usb_cdc.console
 
 ## Loop
 while True:
+    # when the connection is there
     if serial.connected:
+        # when there is new data
         if serial.in_waiting:
-            print(serial.connected)
             print(serial.in_waiting)
-            serial.read(serial.in_waiting)
+            byte_in = serial.read(1)
+            print(byte_in)
+            
+
+            # probably a state machine
+            # when the first byte means to move
+            if byte_in == 99:
+                # do something
+                print("move")
+
 
     time.sleep(1)
